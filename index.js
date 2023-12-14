a7ccce0dba87cb9ebb4ba7394113135f28bd3a3c
Aytac Karkin <152106942+Aytac-Karkin@users.noreply.github.com>
2023-12-14 09:50:45 +0100

    add markbook

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // phew… not a lot going on here. Please add some code!
+
+const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
