d56998c7f4bf2b4cb1d231a85a24b7ba27f0b902
Aytac Karkin <152106942+Aytac-Karkin@users.noreply.github.com>
2023-12-14 15:49:52 +0100

    add toggleAnswer

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
 bookmarkButton.addEventListener("click", () => {
   bookmarkButton.classList.toggle("bookmark--active");
 });
+
+function toggleAnswer () {
+    var answer = document.getElementById("answer");
+    if (answer.style.display === "none") {
+        answer.style.display = "block";
+    } else {
+        answer.style.display = "none"
+    }
+}
